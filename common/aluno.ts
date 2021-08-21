9c37e0ff238e3fcdf706ea76df3041e8161ccc8a
Felipe França <faf2@cin.ufpe.br>
2021-08-21 18:11:07 -0300

    calculaMedia() in aluno.ts

--- common/aluno.ts
+++ common/aluno.ts
@@ -2,8 +2,9 @@ export class Aluno {
   nome: string;
   cpf: string;
   email: string;
-  metas: Map<string,string>;
-  notas: Map<string,number>; 
+  metas: Map<string, string>;
+  notas: Map<string, number>;
+  media: number;
 
   constructor() {
     this.clean();
@@ -13,8 +14,9 @@ export class Aluno {
     this.nome = "";
     this.cpf = "";
     this.email = "";
-    this.metas = new Map<string,string>();
-    this.notas = new Map<string,number>();
+    this.metas = new Map<string, string>();
+    this.notas = new Map<string, number>();
+    this.media = undefined;
   }
 
   clone(): Aluno {
@@ -29,19 +31,36 @@ export class Aluno {
     this.email = from.email;
     this.copyMetasFrom(from.metas);
     this.copyNotasFrom(from.notas);
+    this.media = from.media;
   }
 
-  copyMetasFrom(from: Map<string,string>): void {
-    this.metas = new Map<string,string>();
+  copyMetasFrom(from: Map<string, string>): void {
+    this.metas = new Map<string, string>();
     for (let key in from) {
       this.metas[key] = from[key];
     }
   }
-  
-  copyNotasFrom(from: Map<string,number>): void {
-    this.notas = new Map<string,number>();
+
+  copyNotasFrom(from: Map<string, number>): void {
+    this.notas = new Map<string, number>();
     for (let key in from) {
       this.notas[key] = from[key];
     }
+  }
+
+  calculaMedia(): number {
+    var media: number = 0.0;
+    var unidades: number = 0;
+    for (let key in this.notas) {
+      media += this.notas[key];
+      unidades++;
+    }
+    if (unidades == 0) {
+      media = undefined;
+    } else {
+      media = media / unidades;
+    }
+    this.media = media;
+    return media;
   }
 }
